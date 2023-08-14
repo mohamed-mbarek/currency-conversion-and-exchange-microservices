1ea15480eb5bd89c0d641e5fb6241720c36f69ea
mohamed-mbarek <mbarekmohamed99@gmail.com>
2023-08-14 22:57:07 +0100

    configure ZIPKIn

--- currency-conversion-service/src/main/java/com/in28minutes/rest/webservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/in28minutes/rest/webservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -2,6 +2,9 @@ package com.in28minutes.rest.webservices.currencyconversionservice.controller;
 
 import com.in28minutes.rest.webservices.currencyconversionservice.beans.CurrencyConversion;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,11 +15,23 @@ import com.in28minutes.rest.webservices.currencyconversionservice.service.Curren
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+
+@Configuration(proxyBeanMethods = false)
+class RestTemplateConfiguration {
+
+    @Bean
+    RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+}
 @RestController
 public class CurrencyConversionController {
 
     @Autowired
     private CurrencyExchangeProxy proxy;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(@PathVariable String from,
--- currency-exchange-service/src/main/java/com/in28minutes/rest/webservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/in28minutes/rest/webservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.in28minutes.rest.webservices.currencyexchangeservice.controller;
 
 import com.in28minutes.rest.webservices.currencyexchangeservice.bean.CurrencyEchange;
 import com.in28minutes.rest.webservices.currencyexchangeservice.repository.CurrencyExchangeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +14,7 @@ import java.math.BigDecimal;
 
 @RestController
 public class CurrencyExchangeController {
-
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
     @Autowired
     private Environment environment;
     @Autowired
@@ -21,6 +23,7 @@ public class CurrencyExchangeController {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyEchange retrieveExchangeValue(@PathVariable String from , @PathVariable String to){
         String port = environment.getProperty("local.server.port");
+        logger.info("retrieveExchangeValue  called with {} to {}",from,to);
         CurrencyEchange currencyEchange = currencyExchangeRepository.findByFromAndTo(from, to);
         if(currencyEchange==null)
             throw new RuntimeException("Unable To find data from "+from+" to "+to);
